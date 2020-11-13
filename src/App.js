211f91065f87f047ceec8f6b2f108f8d6a363fd1
Massao <tani22massao@gmail.com>
2020-11-13 14:20:47 -0300

    getting access token

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <button><a href="http://localhost:8888">Logar com Spotify</a></button>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    const parametros = this.getHashParams();
+    const token = parametros.access_token;
+    console.log(token)
+  }
+
+  getHashParams() {
+    var hashParams = {};
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    while ((e = r.exec(q))) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button>
+          <a href="http://localhost:8888">Logar com Spotify</a>
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
