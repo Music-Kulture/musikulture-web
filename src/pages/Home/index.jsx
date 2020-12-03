858cbe2a1f8f603f3d1b5e1803e9557700d65817
Gabriele Seiler Spada <g.seilerspada@usp.br>
2020-12-03 14:01:34 -0300

    estilo home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import "./home.css";
 
 const Home = () => {
     return (
-        <div className="Home">
-            <button>
-            <a href="http://localhost:8888/login">Logar com Spotify</a>
-            </button>
+        <div className="bg">
+            <a href="http://localhost:8888/login" class="btn btn-success btn-lg" type="button">Logar no Spotify</a>
         </div>
     );
 }
